473cc381cad14003d874ebfc63000e3756c7e9e5
wahyuanang <wahyuanng@students.amikom.ac.id>
2024-09-26 09:25:36 +0700

    add folder js and move file from  scripts to JS

---
+++ JS/filterCarByAvailability.js
@@ -0,0 +1,18 @@
+function filterCarByAvailability(cars) {
+  // Sangat dianjurkan untuk console.log semua hal hehe
+  console.log(cars);
+
+  // Tempat penampungan hasil
+  const result = [];
+
+  // Tulis code-mu disini
+  for (let i = 0; i < cars.length; i++) {
+    let car = cars[i];
+    if (car.available) {
+      result.push(car);
+    }
+  }
+
+  // Rubah code ini dengan array hasil filter berdasarkan availablity
+  return result;
+}
